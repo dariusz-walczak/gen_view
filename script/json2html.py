1ef43c1648223057e75ecc6af83e92073ca0ee95
Dariusz Walczak <dariusz.walczak@gmail.com>
2025-05-02 10:09:32 +0200

    Eliminated multiple pylint warnings

--- script/json2html.py
+++ script/json2html.py
@@ -2,13 +2,8 @@
 
 import argparse
 import json
-import os.path
-import re
 import sys
 
-import markdown
-
-from parse import capitalize_word_list, parse_doc_index, parse_doc_name, parse_source
 from render import init_jinja_env
 
 def parse_options(args):
@@ -20,30 +15,12 @@ def parse_options(args):
         "-v", "--doc-version", action="store", metavar="NUM", dest="doc_version",
         help="The document version NUMber")
     parser.add_argument(
-        "context", action="store", metavar="CONTEXT_FILE", help="jinja2 CONTEXT input FILE in json format")
+        "context", action="store", metavar="CONTEXT_FILE",
+        help="jinja2 CONTEXT input FILE in json format")
     parser.add_argument(
         "template", action="store", metavar="TEMPLATE_NAME", help="jinja2 TEMPLATE NAME")
 
     return parser.parse_args(args)
-
-
-def determine_auto_props(file_name):
-    file_name = os.path.basename(file_name)
-    index = parse_doc_index(file_name)
-    full_title_raw = parse_doc_name(file_name)
-
-    # Split the full title into subtitles
-    subtitle_raw_list = re.split('_+', full_title_raw)
-    subtitle_list = []
-
-    for subtitle_raw in subtitle_raw_list:
-        word_list = re.split('-+', subtitle_raw)
-        subtitle_list.append(' '.join(capitalize_word_list(word_list)))
-
-    return {
-        "index": index,
-        "title": ': '.join(subtitle_list)
-    }
 
 
 def main(options):
