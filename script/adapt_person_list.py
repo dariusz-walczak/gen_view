e2d1f1323fc801ad807dfa1f6b1c99dfaddc8e49
Dariusz Walczak <dariusz.walczak@gmail.com>
2025-06-15 15:25:44 +0200

    Added support for the real live years in the person list page

--- script/adapt_person_list.py
+++ script/adapt_person_list.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
+import datetime
 import json
+import logging
 import os.path
 import sys
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(messages)",
+    stream=sys.stderr)
+
+_LOG = logging.getLogger()
+
 
 def parse_options(args):
     parser = argparse.ArgumentParser()
@@ -13,6 +25,57 @@ def parse_options(args):
         help="Person list INPUT FILE path in JSON format")
 
     return parser.parse_args(args)
+
+
+def extract_year_from_iso_date(ctx, field_name):
+    person_id = ctx.get("id", "P?????")
+
+    try:
+        iso_birth_date = ctx[field_name]
+    except KeyError as e:
+        _LOG.debug(
+            "Failed to extract the '%s' field from the person ('%s') context:\n    %s",
+            field_name, person_id, str(e))
+        return None
+
+    try:
+        parsed_date = datetime.date.fromisoformat(iso_birth_date)
+    except (TypeError, ValueError) as e:
+        _LOG.debug(
+            "Failed to convert the value (%s) of the '%s' field of the person ('%s') context:\n"
+            "    %s", iso_birth_date, field_name, person_id, str(e))
+        return None
+
+    return parsed_date.year
+
+
+def adapt_person_details(person_ctx: dict) -> dict:
+    """Adapt the input person context to include all useful, derived data.
+
+    Args:
+        person_ctx: The person context to be adapted
+
+    Returns:
+        dict: An adapted copy of the input person context
+    """
+    # Consider removing the following deep copy when there is a need to optimize:
+    person_ctx = copy.deepcopy(person_ctx)
+
+    birth_year = extract_year_from_iso_date(person_ctx, "birth_date")
+    if birth_year is not None:
+        person_ctx["birth_year"] = birth_year
+
+    death_year = extract_year_from_iso_date(person_ctx, "death_date")
+    if death_year is not None:
+        person_ctx["death_year"] = death_year
+
+    return person_ctx
+
+
+def adapt_each_person(context: list[dict]) -> list[dict]:
+    """Adapt each person context in the input list to include all useful, derived data.
+    """
+    return [adapt_person_details(p) for p in context]
 
 
 def adapt_person_list(context):
@@ -58,7 +121,8 @@ def main(options):
 
     print(
         json.dumps(
-            adapt_person_list(context),
+            adapt_person_list(
+                adapt_each_person(context)),
             indent=4))
 
     return 0
