396e2e8cc18c8723366ddf7b08c781dc75ea717a
Dariusz Walczak <52116076+dariusz-walczak@users.noreply.github.com>
2025-11-09 16:22:42 +0100

    OWL-105: Improved the person notes presentation

--- script/notes.py
+++ script/notes.py
@@ -1,11 +1,6 @@
 import jinja2
 import markupsafe
-
-_note_type_lut = {
-    "info": "Ⓘ",
-    "warning": "Ⓦ",
-    "error": "Ⓧ"
-}
+from markupsafe import Markup
 
 def _render_resource(full_ctx, res_ctx):
     # OWL-101: Throws jinja2.exceptions.TemplateNotFound:
@@ -35,19 +30,32 @@ def _render_var_val(full_ctx, val_ctx):
     return render_cb(full_ctx, val_ctx["value"])
 
 
+_NOTE_EXTRA_CLASS = {
+    "info": "person-notes__note--info",
+    "warning": "person-notes__note--warning",
+    "error": "person-notes__note--error"
+}
+
+
 @jinja2.pass_context
-def render_note(full_ctx, note_ctx) -> markupsafe.Markup:
-    note_type = _note_type_lut.get(note_ctx["type"], "")
-
-    if note_type is None:
-        # OWL-101: Proper error handling needed
-        raise ValueError(f"Invalid note type: {note_ctx['type']}")
-
-    msg_fmt = note_type + " " + _(f"GEN-PERSON-NOTE_{note_ctx['id']}")
-
+def render_note(full_ctx, note_ctx) -> Markup:
+    msg_fmt = _(f"GEN-PERSON-NOTE_{note_ctx['id']}")
     rendered_vars = {
         name: _render_var_val(full_ctx, val_ctx)
         for name, val_ctx in note_ctx["vars"].items()
     }
+    message = msg_fmt.format(**rendered_vars)
 
-    return markupsafe.Markup(msg_fmt.format(**rendered_vars))
+    extra_class = _NOTE_EXTRA_CLASS.get(note_ctx.get("type", "info"), _NOTE_EXTRA_CLASS["info"])
+
+    ctx = {
+        "config": full_ctx["config"],
+        "note": {
+            "extra_class": extra_class,
+            "message": markupsafe.Markup(message)
+        }
+    }
+
+    # OWL-101: Throws jinja2.exceptions.TemplateNotFound:
+    template = full_ctx.environment.get_template("notes/note.html")
+    return markupsafe.Markup(template.render(ctx))
